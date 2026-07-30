f4fda0e10af7e5e9c351fa964a7da5dcbd61959a
GabrielRayat-dev <gabrielrayatofficial@gmail.com>
2026-07-30 21:48:13 +0800

    feat: add wildcard CORS support for Render deployment

--- server/src/app.js
+++ server/src/app.js
@@ -14,7 +14,7 @@ const authLimiter = rateLimit({
 
 // Middlewares
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: process.env.CLIENT_URL === '*' ? '*' : (process.env.CLIENT_URL || 'http://localhost:5173'),
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
